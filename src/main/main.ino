a0fc26b926b0a49c6bbc934257c6b503cd778f6a
Luiz <luizcn123@hotmail.com>
2023-11-29 13:06:01 -0300

    adiciona coleta :fire:

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,71 +1,28 @@
-#include <Wire.h>
-#include <Arduino_JSON.h>
+#include<Wire.h>
 
+// Endereco I2C do sensor MPU-6050
 const int MPU = 0x68;
 
-int leituras_pos_x[100];
-int leituras_neg_x[100];
+// Variaveis para armazenar valores do sensor
+float AccX, AccY, AccZ, Temp, GyrX, GyrY, GyrZ;
 
-int leituras_pos_y[100];
-int leituras_neg_y[100];
-
-int leituras_pos_z[100];
-int leituras_neg_z[100];
-
+float aceleracao[100];
 int cont = 0;
 
-void adicionarLeituraX(int novaLeitura, char signal) {
-  if (signal == 'p') {
-    for (int i = 99; i > 0; i--) {
-      leituras_pos_x[i] = leituras_pos_x[i - 1];
-    }
-    leituras_pos_x[0] = novaLeitura;
-  } else if (signal == 'n') {
-    for (int i = 99; i > 0; i--) {
-      leituras_neg_x[i] = leituras_neg_x[i - 1];
-    }
-    leituras_neg_x[0] = novaLeitura;
+void plota(){
+  Serial.print("Vou plotar os dados\n\n");
+  for(int i = 0; i<100; i++){
+    Serial.println(aceleracao[i]);
   }
-}
-
-void adicionarLeituraY(int novaLeitura, char signal) {
-  if (signal == 'p') {
-    for (int i = 99; i > 0; i--) {
-      leituras_pos_y[i] = leituras_pos_y[i - 1];
-    }
-    leituras_pos_y[0] = novaLeitura;
-  } else if (signal == 'n') {
-    for (int i = 99; i > 0; i--) {
-      leituras_neg_y[i] = leituras_neg_y[i - 1];
-    }
-    leituras_neg_y[0] = novaLeitura;
-  }
-}
-
-void adicionarLeituraZ(int novaLeitura, char signal) {
-  if (signal == 'p') {
-    for (int i = 99; i > 0; i--) {
-      leituras_pos_z[i] = leituras_pos_z[i - 1];
-    }
-    leituras_pos_z[0] = novaLeitura;
-  } else if (signal == 'n') {
-    for (int i = 99; i > 0; i--) {
-      leituras_neg_z[i] = leituras_neg_z[i - 1];
-    }
-    leituras_neg_z[0] = novaLeitura;
-  }
-}
-
-float calcularMedia(int leituras[100]) {
-  int total = 0;
-  for (int i = 0; i < 100; i++) {
-    total += leituras[i];
-  }
-  return total / 100.0;
+  Serial.print("Terminei de plotar\n\n");
+  delay(100000);
 }
 
 void setup() {
+  // Inicializa Serial
   Serial.begin(9600);
+
+  // Inicializa o MPU-6050
   Wire.begin();
   Wire.beginTransmission(MPU);
   Wire.write(0x6B);
@@ -73,55 +30,93 @@ void setup() {
   Wire.endTransmission(true);
 
   // Configura Giroscópio para fundo de escala desejado
+  /*
+    Wire.write(0b00000000); // fundo de escala em +/-250°/s
+    Wire.write(0b00001000); // fundo de escala em +/-500°/s
+    Wire.write(0b00010000); // fundo de escala em +/-1000°/s
+    Wire.write(0b00011000); // fundo de escala em +/-2000°/s
+  */
   Wire.beginTransmission(MPU);
   Wire.write(0x1B);
-  Wire.write(0b00011000);
+  Wire.write(0x00000000);  // Trocar esse comando para fundo de escala desejado conforme acima
   Wire.endTransmission();
 
   // Configura Acelerometro para fundo de escala desejado
+  /*
+      Wire.write(0b00000000); // fundo de escala em +/-2g
+      Wire.write(0b00001000); // fundo de escala em +/-4g
+      Wire.write(0b00010000); // fundo de escala em +/-8g
+      Wire.write(0b00011000); // fundo de escala em +/-16g
+  */
   Wire.beginTransmission(MPU);
   Wire.write(0x1C);
-  Wire.write(0b00011000);
+  Wire.write(0b00011000);  // Trocar esse comando para fundo de escala desejado conforme acima
   Wire.endTransmission();
+  Serial.print("Preparando para coletar as 100 amostras\n");
+  delay(10000);
 }
 
 void loop() {
+  // Comandos para iniciar transmissão de dados
   Wire.beginTransmission(MPU);
   Wire.write(0x3B);
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU, 14, true);
+  Wire.requestFrom(MPU, 14, true); // Solicita os dados ao sensor
 
-  int AccX = Wire.read() << 8 | Wire.read();
-  int AccY = Wire.read() << 8 | Wire.read();
-  int AccZ = Wire.read() << 8 | Wire.read();
-  int Temp = Wire.read() << 8 | Wire.read();
-  int GyrX = Wire.read() << 8 | Wire.read();
-  int GyrY = Wire.read() << 8 | Wire.read();
-  int GyrZ = Wire.read() << 8 | Wire.read();
+  // Armazena o valor dos sensores nas variaveis correspondentes
+  AccX = Wire.read() << 8 | Wire.read(); //0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  AccY = Wire.read() << 8 | Wire.read(); //0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AccZ = Wire.read() << 8 | Wire.read(); //0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Temp = Wire.read() << 8 | Wire.read(); //0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyrX = Wire.read() << 8 | Wire.read(); //0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyrY = Wire.read() << 8 | Wire.read(); //0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyrZ = Wire.read() << 8 | Wire.read(); //0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 
-  // Adiciona leituras aos vetores correspondentes
-  adicionarLeituraX(AccX, (AccX >= 0) ? 'p' : 'n');
-  adicionarLeituraY(AccY, (AccY >= 0) ? 'p' : 'n');
-  adicionarLeituraZ(AccZ, (AccZ >= 0) ? 'p' : 'n');
-  Serial.println(AccX);
+  // Imprime na Serial os valores obtidos
+  /* Alterar divisão conforme fundo de escala escolhido:
+      Acelerômetro
+      +/-2g = 16384
+      +/-4g = 8192
+      +/-8g = 4096
+      +/-16g = 2048
 
-  cont++;
+      Giroscópio
+      +/-250°/s = 131
+      +/-500°/s = 65.6
+      +/-1000°/s = 32.8
+      +/-2000°/s = 16.4
+  */
 
-  // Coleta 100 amostras para cada eixo
-  if (cont == 100) {
-    // Calcula médias
-    float media_pos_x = calcularMedia(leituras_pos_x);
-    float media_neg_x = calcularMedia(leituras_neg_x);
-    float media_pos_y = calcularMedia(leituras_pos_y);
-    float media_neg_y = calcularMedia(leituras_neg_y);
-    float media_pos_z = calcularMedia(leituras_pos_z);
-    float media_neg_z = calcularMedia(leituras_neg_z);
-    Serial.print("Media: ");
-    Serial.println(media_neg_x);
+  Serial.print(AccX / 2048);
+  Serial.print(" ");
+  Serial.print(AccY / 2048);
+  Serial.print(" ");
+  Serial.println(AccZ / 2048);
 
-    cont = 0;
-    delay(10000);
+
+  // Salva as 100 amostras positivas em +- 1g
+  // Para trocar o eixo basta mudo Acc<eixo> para 'x', 'y' ou 'z'
+  if ((AccX / 2048) >= 0 && (cont < 100)){
+    Serial.print("Entrei no if, estou coletando dados\n\n");
+    aceleracao[cont] = AccX/2048;
+    cont += 1;
   }
+  if (cont == 100){
+    cont = 0;
+    Serial.print("Entrei no plot\n");
+    plota();
+  }
+  
 
-  delay(50); 
+/*
+  Serial.print(GyrX / 131);
+  Serial.print(" ");
+  Serial.print(GyrY / 131);
+  Serial.print(" ");
+  Serial.println(GyrZ / 131);
+*/
+
+
+ // Atraso de 100ms
+  delay(100);
 }
